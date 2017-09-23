b170444d4797e7ffa9a30f53b227772353f187a8
Andriy <andy25771@gmail.com>
2017-09-23 13:30:11 +0300

    "added JSON"

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ЕнеЇда', 
+            quotes: data});
 });
 
 module.exports = router;
