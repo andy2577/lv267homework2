fb0ecf81fe402a27cb45288305deeca4ae724e2e
Andriy <andy25771@gmail.com>
2017-09-27 10:38:26 +0300

    postmen test

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,26 @@
 var express = require('express');
-var https = require('https');
 var router = express.Router();
 // var data = require('../data');
-var data = '';
-
-let apireq = (req, res) => {
-  const options = {
-    hostname: 'lv267homework2-api.herokuapp.com',
-    port: 443,
-    path: '/',
-    method: 'GET'
-  };
-
-  const ajxreq = https.request(options, (ajxres) => {
-    // console.log('statusCode:', ajxres.statusCode);
-    // console.log('headers:', ajxres.headers);
-
-    ajxres.on('data', (d) => {
-      // process.stdout.write(d);
-      data = JSON.parse(d);
-      // console.log(req.params.student);
-      res.render('index', {
-        title: 'ЕнеЇда',
-        quotes: data,
-        show: req.params.student
-      });
-
-    });
-  });
-
-  ajxreq.on('error', (e) => {
-    console.error(e);
-  });
-
-  ajxreq.end();
-}
+var indexController = require('../controllers/indexController');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  apireq(req, res);
+router.get('/', indexController);
+
+router.get('/:student', indexController);
+
+router.post('/student', function(req, res, next){
+    res.send(req.body);
+    console.log(req.body);
 });
 
-router.get('/:student', function (req, res, next) {
-  apireq(req, res);
+router.put('/student/:id', function(req, res, next){
+    res.send(req.body);
+    console.log(req.body);
+});
+
+router.delete('/student/:id', function(req, res, next){
+    res.send(req.body);
+    console.log(req.body);
 });
 
 module.exports = router;
