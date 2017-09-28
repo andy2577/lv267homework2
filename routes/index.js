43936b2ff6b061cbf4887119d8e52d7bcf28925f
Andriy <andy25771@gmail.com>
2017-09-28 09:23:53 +0300

    working delete/create/read

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,26 @@ var express = require('express');
 var router = express.Router();
 // var data = require('../data');
 var indexController = require('../controllers/indexController');
+var saveController = require('../controllers/saveController');
+var deleteController = require('../controllers/deleteController');
 
 /* GET home page. */
 router.get('/', indexController);
 
-router.get('/:student', indexController);
+// router.delete('/:delete', deleteController);
+router.post('/delete/:delete', deleteController);
 
-router.post('/student', function(req, res, next){
-    res.send(req.body);
-    console.log(req.body);
-});
+router.post('/newquote', saveController);
 
-router.put('/student/:id', function(req, res, next){
-    res.send(req.body);
-    console.log(req.body);
-});
+// router.get('/student/:id', function(req, res, next){
+//     res.send(req.params.id);
+//     console.log(req.params.id);
+// });
 
-router.delete('/student/:id', function(req, res, next){
-    res.send(req.body);
-    console.log(req.body);
-});
+// router.delete('/student/:id', function(req, res, next){
+//     res.send(req.body);
+//     console.log(req.body);
+// });
 
 
 module.exports = router;
