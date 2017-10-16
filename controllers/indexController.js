215017694e0528a688c3d53847cd9c970f972829
andy2577 <apetrushchak@gmail.com>
2017-10-16 21:02:52 +0300

    removed some comments

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,14 +1,11 @@
 var Eneida = require('../models/book');
 
 module.exports = (req, res, next) => {
-  
         Eneida.find((err, dbres) =>{
-
             if(err) return err;
-
             var options = {
                 quotes: dbres,
-                show: false,//hack for testing TODO proper flag id
+                show: false,
                 title: 'ЕнеЇда'
             }
             res.render('index', options);
